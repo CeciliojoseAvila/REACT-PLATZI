0217403174156df00a5af257847d95f4e179be68
Cecilio Avila <ceciliojose2@hotmail.com>
2023-05-07 22:08:04 -0500

    OK. APP PLATZI

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+    <div>
+      Error 404. Esta página no existe
+      <img src="...../src/assets/logos/404.gif" alt="menu" className="menu" />
+    </div>
+  );
+}
+
+export default NotFound;
